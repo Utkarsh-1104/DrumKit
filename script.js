28ddbfe0bf5aeab670e2816c5e1d343e2a3ced60
LONEWOLF99 <bajpaiutkarsh1104@gmail.com>
2023-11-11 16:03:42 +0530

    ..

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let noOfDrums = document.querySelectorAll(".drum").length
 for(let i=0; i < noOfDrums; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-        
         let clkBtn = this.innerHTML;
         makeSound(clkBtn);
         btnAnimation(clkBtn);
